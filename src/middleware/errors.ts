e0317be50b98c00ab8340761e668b14d8c6e7321
Zhenzhi <zhenzhi97@gmail.com>
2022-11-05 12:11:52 +0100

    Add CustomError and error middleware and its tests

---
+++ src/middleware/errors.ts
@@ -0,0 +1,25 @@
+import debugCreator from "debug";
+import type { NextFunction, Request, Response } from "express";
+import type CustomError from "../CustomError/CustomError";
+
+const debug = debugCreator("robots:errors");
+
+export const generalError = (
+  error: CustomError,
+  req: Request,
+  res: Response,
+  // eslint-disable-next-line no-unused-vars
+  next: NextFunction
+) => {
+  debug(`There was an error: ${error.message}`);
+  const statusCode = error.status ?? 500;
+  const publicMessage =
+    error.publicMessage || "Failed to connect to the server.";
+
+  res.status(statusCode).json({ message: publicMessage });
+};
+
+export const unknownEndpoint = (req: Request, res: Response) => {
+  debug("Unknow endpoint");
+  res.status(404).json({ message: "Unknown endpoint" });
+};
