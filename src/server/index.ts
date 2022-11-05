eadbee88cc096fa37b628d9b9d12a88725fa3cb1
Zhenzhi <zhenzhi97@gmail.com>
2022-11-05 15:23:41 +0100

    Fix unintentional removed app.use of errors

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,11 +2,15 @@ import "../loadEnvironment.js";
 import "../database/databaseConnection.js";
 import robotsRouter from "./routers/robotsRouter.js";
 import express from "express";
+import { generalError, unknownEndpoint } from "../middleware/errors.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/robots", robotsRouter);
+
+app.use(generalError);
+app.use(unknownEndpoint);
 
 export default app;
