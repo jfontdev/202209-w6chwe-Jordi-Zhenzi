b96f285116fa890ecc7b18b89422ff29afc8a89a
Zhenzhi <zhenzhi97@gmail.com>
2022-11-05 18:53:15 +0100

    Add getRobotById test

--- src/server/controllers/robotsControllers.test.ts
+++ src/server/controllers/robotsControllers.test.ts
@@ -1,21 +1,24 @@
 import type { Request, Response } from "express";
 import Robot from "../../database/models/Robot";
-import { getRobots } from "./robotsControllers";
+import { getRobotById, getRobots } from "./robotsControllers";
 
-describe("Given a  Robots controller", () => {
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+const res: Partial<Response> = {
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn(),
+};
+
+describe("Given a getRobots controller", () => {
   describe("When it receives a response", () => {
     test("Then it should respond with a status code 200", async () => {
       const expectedStatus = 200;
 
-      const req: Partial<Request> = {};
-      const res: Partial<Response> = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn(),
-      };
-
       Robot.find = jest.fn();
 
-      await getRobots(req as Request, res as Response);
+      await getRobots(null, res as Response);
 
       expect(res.status).toHaveBeenLastCalledWith(expectedStatus);
     });
@@ -32,17 +35,76 @@ describe("Given a  Robots controller", () => {
         },
       ];
 
-      const req: Partial<Request> = {};
-      const res: Partial<Response> = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn(),
-      };
-
       Robot.find = jest.fn().mockReturnValue(robotList);
 
-      await getRobots(req as Request, res as Response);
+      await getRobots(null, res as Response);
 
       expect(res.json).toHaveBeenCalledWith({ robots: robotList });
+    });
+  });
+});
+
+describe("Given a getRobotById controller", () => {
+  const robot = [
+    {
+      robots: {
+        features: {
+          speed: 2,
+          dateOfCreation: "2022-10-21T22:00:00.000Z",
+          endurance: 4,
+        },
+        _id: "63654c8a95408f6adaf32e5a",
+        image: "url",
+        name: "Robot1",
+      },
+    },
+  ];
+
+  Robot.findById = jest.fn().mockReturnThis();
+  Robot.findById = jest.fn().mockReturnValue(robot[0]);
+
+  describe("When it receives a existing ID", () => {
+    const req = {
+      params: "" as unknown,
+    };
+
+    test("Then the status method should be called with a 200", async () => {
+      const expectedStatus = 200;
+
+      await getRobotById(req as Request, res as Response, null);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatus);
+    });
+
+    test("Then the json method should be called with a robot", async () => {
+      await getRobotById(req as Request, res as Response, null);
+
+      expect(res.json).toHaveBeenCalledWith({ robots: robot[0] });
+    });
+  });
+
+  describe("When it receives a non-existing ID", () => {
+    const req = {
+      params: "" as unknown,
+    };
+
+    test("Then the status method should be called with a 404", async () => {
+      const expectedStatus = 404;
+
+      Robot.findById = jest.fn().mockReturnValue(robot[2]);
+      await getRobotById(req as Request, res as Response, null);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatus);
+    });
+
+    test("Then the json method should be called with 'That robot was recycled and does not exist anymore.'", async () => {
+      const expectedError = {
+        message: "That robot was recycled and does not exist anymore.",
+      };
+
+      await getRobotById(req as Request, res as Response, null);
+
+      expect(res.json).toHaveBeenCalledWith(expectedError);
     });
   });
 });
--- src/server/controllers/robotsControllers.ts
+++ src/server/controllers/robotsControllers.ts
@@ -1,7 +1,8 @@
 import debugCreator from "debug";
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import Robot from "../../database/models/Robot.js";
 import chalk from "chalk";
+import CustomError from "../../CustomError/CustomError.js";
 
 const debug = debugCreator("robots:server:controllers:robotsController");
 
@@ -12,13 +13,31 @@ export const getRobots = async (req: Request, res: Response) => {
   debug(chalk("The robot list have been succesfully sended"));
 };
 
-export const getRobotById = async (req: Request, res: Response) => {
+export const getRobotById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { idRobot } = req.params;
+
   try {
-    const robotList = await Robot.findById(idRobot);
-    res.status(200).json({ robots: robotList });
-  } catch {
-    debug("That robot was recycled and does not exist anymore.");
-    res.status(404).json("Robot not found by that ID.");
+    const robot = await Robot.findById(idRobot);
+
+    if (!robot) {
+      res.status(404).json({
+        message: "That robot was recycled and does not exist anymore.",
+      });
+      return;
+    }
+
+    debug({ robots: robot });
+    res.status(200).json({ robots: robot });
+  } catch (error: unknown) {
+    const idError = new CustomError(
+      (error as Error).message,
+      500,
+      "Robot not found by that ID."
+    );
+    next(idError);
   }
 };
